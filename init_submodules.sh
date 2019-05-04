2cde1e9785da46ee0a9cdbee51e5be1f477cb669
Marko Kosunen <marko.kosunen@aalto.fi>
2019-05-03 18:04:59 -0700

    Initial commit

---
+++ init_submodules.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname $0 )" && pwd )"
+cd $DIR
+
+for module in \
+    ./chisel; do
+    git submodule update --init $module
+    cd ./$module
+    ./init_submodules.sh
+    sbt publishLocal
+    cd $DIR
+done
+
+exit 0
+
+
+

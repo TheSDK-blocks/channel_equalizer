395f920aaf5d1030f5a580073ae814ab09139ff8
Marko Kosunen <marko.kosunen@aalto.fi>
2019-05-07 19:53:55 -0700

    Bump

--- channel_equalizer/__init__.py
+++ channel_equalizer/__init__.py
@@ -10,34 +10,48 @@ import tempfile
 
 from thesdk import *
 from verilog import *
+from verilog.testbench import *
+from verilog.testbench import testbench as vtb
 
-class channel_equalizer(verilog,vhdl,thesdk):
+
+class channel_equalizer(verilog,thesdk):
+    @property
+    def _classfile(self):
+        return os.path.dirname(os.path.realpath(__file__)) + "/"+__name__
+
     def __init__(self,*arg): 
-        self.proplist = [ 'Rs' ];    #properties that can be propagated from parent
-        self.Rs = 1;                 # sampling frequency
-        self.iptr_A = refptr();      # Pointer for input data
+        self.proplist = [ 'Rs', 'symbol_length', 'Users' ];    #properties that can be propagated from parent
+        self.Rs = 160e6;                   # Sampling frequency
+        self.Users = 16;                   # Number of users
+        self.symbol_length  = 64;          # OFDM symbol length
+        self.A = IO();            # Input data, FFT Bins in series 
+        self._Z = IO();           # Output, equalized FFT bins
+        self.control_in = IO()
+        self.control_in.Data = Bundle()
+        self.control_out = IO()
+        self.control_out.Data = Bundle()
         self.model='py';             #can be set externally, but is not propagated
         self.par= False              #By default, no parallel processing
         self.queue= []               #By default, no parallel processing
-        self._Z = refptr();          # Pointer for output data
-        #Classfile is required by verilog and vhdl classes to determine paths.
-        self._classfile=os.path.dirname(os.path.realpath(__file__)) + "/"+__name__
+
         if len(arg)>=1:
             parent=arg[0]
             self.copy_propval(parent,self.proplist)
             self.parent =parent;
         self.init()
     def init(self):
-        self._vlogparameters =dict([('Rs',100e6)])
-        self.def_verilog()
-        self._vhdlparameters =dict([('Rs',100e6)])
-        self.def_vhdl()
+        #This gets updated every time you add an iofile
+        self.iofile_bundle=Bundle()
+        #Adds files to bundle
+        _=verilog_iofile(self,name='Z',datatype='complex')
+        _=verilog_iofile(self,name='A',dir='in')
+        self.vlogparameters=dict([ ('g_Rs',self.Rs),])
 
     def main(self):
-        out=np.array(self.iptr_A.Value)
+        out=np.array(self.A.Data)
         if self.par:
             self.queue.put(out)
-        self._Z.Value=out
+        self._Z.Data=out
 
     def run(self,*arg):
         if len(arg)>0:
@@ -46,46 +60,247 @@ class channel_equalizer(verilog,vhdl,thesdk):
         if self.model=='py':
             self.main()
         else: 
-          self.write_infile()
           if self.model=='sv':
-              self.run_verilog()
+              if not 'control_file' in self.control_in.Data.Members:
+                  self.create_controlfile()
+                  self.reset_sequence()
+              else:
+                  self.control_in.Data.Members['control_file'].adopt(parent=self)
+
+              # Create testbench and execute the simulation
+              self.define_testbench()
+              self.tb.export(force=True)
+              self.write_infile()
+              #self.run_verilog()
+              #self.read_outfile()
+              #del self.iofile_bundle
+
           elif self.model=='vhdl':
-              self.run_vhdl()
-          self.read_outfile()
+              self.print_log(type='F', msg='VHDL model not yet supported')
 
     def write_infile(self):
-        rndpart=os.path.basename(tempfile.mkstemp()[1])
-        if self.model=='sv':
-            self._infile=self._vlogsimpath +'/A_' + rndpart +'.txt'
-            self._outfile=self._vlogsimpath +'/Z_' + rndpart +'.txt'
-        elif self.model=='vhdl':
-            self._infile=self._vhdlsimpath +'/A_' + rndpart +'.txt'
-            self._outfile=self._vhdlsimpath +'/Z_' + rndpart +'.txt'
-        else:
-            pass
-        try:
-          os.remove(self._infile)
-        except:
-          pass
-        fid=open(self._infile,'wb')
-        np.savetxt(fid,np.transpose(self.iptr_A.Value),fmt='%.0f')
-        #np.savetxt(fid,self.iptr_A.Value.reshape(-1,1).view(float),fmt='%i', delimiter='\t')
-        fid.close()
+        #Input file data definitions
+        indata=self.A.Data
+        self.iofile_bundle.Members['A'].data=indata
+        indata=None #Clear variable to save memory
+
+        # This could be a method somewhere
+        for name, val in self.iofile_bundle.Members.items():
+            if val.dir=='in':
+                self.iofile_bundle.Members[name].write()
 
     def read_outfile(self):
-        fid=open(self._outfile,'r')
-        out = np.transpose(np.loadtxt(fid))
-        #out = np.loadtxt(fid,dtype=complex)
-        #Of course it does not work symmetrically with savetxt
-        #out=(out[:,0]+1j*out[:,1]).reshape(-1,1) 
-        fid.close()
-        os.remove(self._outfile)
+        #Handle the ofiles here as you see the best
+        a=self.iofile_bundle.Members['Z']
+        a.read(dtype='object')
+        self._Z.Data=a.data
+        print(self._Z.Data)
         if self.par:
-            self.queue.put(out)
-        self._Z.Value=out
+            self.queue.put(self._Z)
+
+    def create_controlfile(self):
+        self.control_in.Data.Members['control_file']=verilog_iofile(self,
+            name='control_file',
+            dir='in',
+            iotype='ctrl'
+        )
+        # Create connectors of the signals controlled by this file
+        # Connector list simpler to create with intermediate variable
+        c=verilog_connector_bundle()
+        siglist=[
+               'reset',
+               'initdone',
+                ]
+        for i in range(self.Users):
+            siglist += ['io_reference_in_%s_real' %i] 
+            siglist += ['io_reference_in_%s_imag' %i]
+
+        for name in siglist:
+            c.new(name=name, cls='reg')
+
+        self.control_in.Data.Members['control_file']\
+                .verilog_connectors=c.list(names=siglist)
+
+    def reset_sequence(self):
+        #start defining the file
+        f=self.control_in.Data.Members['control_file']
+        f.set_control_data(init=0) #Initialize to zero at time 0
+        time=0
+        for name in [ 'reset', ]:
+            f.set_control_data(time=time,name=name,val=1)
+
+        # After awhile, switch off reset 
+        time=int(16/(self.Rs*1e-12))
+
+        for name in [ 'reset', ]:
+            f.set_control_data(time=time,name=name,val=0)
+
+        for name in [ 'initdone', ]:
+            f.set_control_data(time=time,name=name,val=1)
+
+    # Testbench definition method
+    def define_testbench(self):
+        #Initialize testbench
+        self.tb=vtb(self)
+
+        # Dut is creted automaticaly, if verilog file for it exists
+        self.tb.connectors.update(bundle=self.tb.dut_instance.io_signals.Members)
+
+        #Assign verilog simulation parameters to testbench
+        self.tb.parameters=self.vlogparameters
+
+        # Copy iofile simulation parameters to testbench
+        for name, val in self.iofile_bundle.Members.items():
+            self.tb.parameters.Members.update(val.vlogparam)
+
+        # Define the iofiles of the testbench. '
+        # Needed for creating file io routines 
+        self.tb.iofiles=self.iofile_bundle
+
+        #Define testbench verilog file
+        self.tb.file=self.vlogtbsrc
+
+        # Create TB connectors from the control file
+        for connector in self.control_in.Data.Members['control_file'].verilog_connectors:
+            self.tb.connectors.Members[connector.name]=connector
+            # Connect them to DUT
+            try: 
+                self.dut.ios.Members[connector.name].connect=connector
+            except:
+                pass
+
+        ## Start initializations
+        #Init the signals connected to the dut input to zero
+        for name, val in self.tb.dut_instance.ios.Members.items():
+            if val.cls=='input':
+                val.connect.init='\'b0'
+
+        # IO file connector definitions
+        # Define what signals and in which order and format are read form the files
+        # i.e. verilog_connectors of the file
+        name='control_file'
+        ionames=[]
+        for i in range(self.Users):
+            ionames += ['io_reference_in_%s_real' %i]
+            ionames += ['io_reference_in_%s_imag' %i]
+
+        self.iofile_bundle.Members[name].verilog_connectors=\
+                self.tb.connectors.list(names=ionames)
+        
+        for name in ionames:
+            self.tb.connectors.Members[name].type='signed'
+
+        name='A'
+        ionames=[]
+        ionames+=['io_A_real', 'io_A_imag' ]
+        self.iofile_bundle.Members[name].verilog_connectors=\
+                self.tb.connectors.list(names=ionames)
+
+        name='Z'
+        ionames=[]
+        ionames+=['io_Z_real', 'io_Z_imag' ]
+        self.iofile_bundle.Members[name].verilog_connectors=\
+                self.tb.connectors.list(names=ionames)
+
+        self.generate_tb_contents()
+
+    def generate_tb_contents(self):
+    # Start the testbench contents
+        self.tb.contents="""
+//timescale 1ps this should probably be a global model parameter
+parameter integer c_Ts=1/(g_Rs*1e-12);
+reg done;
+"""+\
+self.tb.connector_definitions+\
+self.tb.iofile_definitions+\
+"""
+
+//DUT definition
+"""+\
+self.tb.dut_instance.instance+\
+"""
+
+//Master clock is omnipresent
+always #(c_Ts/2.0) clock = !clock;
+
+//Execution with parallel fork-join and sequential begin-end sections
+initial #0 begin
+fork
+done=0;
+""" + \
+self.tb.connectors.verilog_inits(level=1)+\
+"""
+//io_out
+$display("Ready to write");
+@(posedge initdone) begin
+    $display("Posedge initdone");
+while (!done) begin
+@(posedge clock ) begin
+    //Print only valid values
+    if ("""+\
+            self.iofile_bundle.Members['Z'].verilog_io_condition +\
+        """) begin
+        """+\
+            self.iofile_bundle.Members['Z'].verilog_io+\
+        """
+     end
+end
+end
+end
+
+    // Sequence triggered by initdone
+    $display("Ready to read");
+    @(posedge initdone ) begin
+    $display("Posedge initdone");
+        while (!$feof(f_A)) begin
+             @(posedge clock )
+             """+\
+             self.iofile_bundle.Members['A'].verilog_io+\
+             """
+        end
+        done<=1;
+    end
+begin
+"""+\
+self.iofile_bundle.Members['control_file'].verilog_io+\
+"""
+end
+    join
+    """+self.tb.iofile_close+"""
+    $finish;
+end"""
 
 if __name__=="__main__":
     import matplotlib.pyplot as plt
     from  channel_equalizer import *
-    t=thesdk()
-    t.print_log({'type':'I', 'msg': "This is a testing template. Enjoy"})
+    dut=channel_equalizer()
+    dut2=channel_equalizer()
+    dut.model='py'
+    dut2.model='sv'
+    #dut2.interactive_verilog=True
+    len=16*64
+    phres=64
+    fsig=25e6
+    indata=2**10*np.exp(1j*2*np.pi/phres*(np.arange(len)*np.round(fsig/dut.Rs*phres)))\
+            .reshape(-1,1)
+    dut.A.Data=indata
+    dut2.A.Data=indata
+
+    dut.run()
+    dut2.run()
+    #f0=plt.figure(0)
+    #plt.plot(np.abs(dut._io_out.Data[10,:]))
+    #plt.suptitle("Python model")
+    #plt.xlabel("Freq")
+    #plt.ylabel("Abs(FFT)")
+    #plt.show(block=False)
+    #f0.savefig('fft_python.eps', format='eps', dpi=300);
+    #f1=plt.figure(1)
+    #plt.plot(np.abs(dut2._io_out.Data[10,:]))
+    #plt.suptitle("Verilog model")
+    #plt.xlabel("Freq")
+    #plt.ylabel("Abs(FFT)")
+    #plt.show(block=False)
+    #f1.savefig('fft_verilog.eps', format='eps', dpi=300);
+    #input()
+
